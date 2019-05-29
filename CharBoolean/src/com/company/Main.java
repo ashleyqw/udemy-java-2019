8f87d561a8ca001ed8a6049a5aa2e7f4bd6e0909
Ashley Wang <ashley@Ashbook-Pro.local>
2019-05-29 17:15:24 -0600

    video 21, char boolean

---
+++ CharBoolean/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // char width is 16 bits = 2 bytes
+        char aChar = '$';
+        char copyrightSymbol = '\u00A9';    // from unicode website
+        System.out.println(copyrightSymbol);
+
+        // booleans can only be true or false
+        boolean isHappy = true;
+
+    }
+}
