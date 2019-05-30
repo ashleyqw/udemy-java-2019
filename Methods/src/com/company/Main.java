de74f232c63cbd331b9d9dfadc0495b83f371f11
Ashley Wang <ashley@Ashbook-Pro.local>
2019-05-30 10:19:44 -0600

    video 30-32, methods

---
+++ Methods/src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+
+        int scorePosition = calculatePosition(highScore);
+        displayScorePosition("Maria", scorePosition);
+
+    }
+
+    // methods
+
+    // after public static should be the type of the thing being returned, in this case int
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            return finalScore;
+        }
+        return -1;
+    }
+
+    // void means returns nothing
+//    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//
+//        if (gameOver) {
+//            int finalScore = score + (levelCompleted * bonus);
+//            finalScore += 1000;
+//            System.out.println("Your final score was " + finalScore);
+//        }
+//
+//    }
+
+    public static void displayScorePosition(String name, int position) {
+        System.out.println(name + " is in position " + position);
+    }
+
+    public static int calculatePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
