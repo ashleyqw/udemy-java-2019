0e453f8536582894cf94699debe17a615db6f37e
Ashley Wang <ashleyqwang@gmail.com>
2019-06-03 23:26:27 -0700

    video 63-71, inheritance

---
+++ Inheritance/src/com/company/Dog.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        super.move(5);
+    }
+
+    public void run() {
+        System.out.println("Dog.walk() called");
+        move(10);   // since this isn't calling the super.move() method, it'll look for a move() method in the Dog class first
+        // it'll use the super.move() one if it doesn't find an override for it in this class
+    }
+
+    private void moveLegs(int speed) {
+        System.out.println("Dog.moveLegs() called at speed " + speed);
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called");
+        moveLegs(speed);
+        super.move(speed);
+    }
+}
